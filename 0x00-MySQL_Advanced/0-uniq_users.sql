f4fda107d27b315d0aaf0cc9b290aede79d73a1c
icis-04 <chidexiwuaku@gmail.com>
2023-01-13 14:38:52 +0000

    initial commit

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- creates a table called users
+
+
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
