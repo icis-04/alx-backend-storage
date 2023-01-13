e47068bc23134943b07a4d6a437c1fa59bcf391c
icis-04 <chidexiwuaku@gmail.com>
2023-01-13 15:09:02 +0000

    secong commit

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table called users
+
+
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US' DEFAULT, 'CO', 'TN')
+	);
