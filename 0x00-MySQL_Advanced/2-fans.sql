846511eec863ac897abdc1755b114a425a92348b
icis-04 <chidexiwuaku@gmail.com>
2023-01-14 17:51:30 -0800

    nb fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- calculates the sum of fans by country
 
 
-SELECT origin, SUM(fans) as 'nb_fans'
+SELECT origin, SUM(fans) AS 'nb_fans'
 FROM metal_bands
 GROUP BY origin
 ORDER BY 'nb_fans' DESC;
